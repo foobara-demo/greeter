c87638439e4e9abc6cbe920e191984935a6369de
Miles Georgi <azimux@gmail.com>
2026-02-03 12:49:02 -0800

    Get improved foobara.rb initializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :greetings, only: [:create], defaults: { format: :json }
+  resources :greetings, only: [ :create ], defaults: { format: :json }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
